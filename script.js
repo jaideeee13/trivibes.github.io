f0bf54fe46c92985fe6158ce93b311f23ba09a0d
jaideeee13 <jadelibao620@gmail.com>
2025-05-08 22:31:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const nav = document.getElementById('nav-list');
+    nav.classList.toggle('show');
+}
+
+function navigateTo(page) {
+    window.location.href = page;
+    
+}
